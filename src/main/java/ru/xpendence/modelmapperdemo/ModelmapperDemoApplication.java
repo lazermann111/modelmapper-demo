00538c26fa88bf81773faf6728fe7564f57a9ba1
lazermann111 <10075519+lazermann111@users.noreply.github.com>
2020-03-06 13:52:11 +0200

    @ConditionalOnProperty + config cleanup

--- src/main/java/ru/xpendence/modelmapperdemo/ModelmapperDemoApplication.java
+++ src/main/java/ru/xpendence/modelmapperdemo/ModelmapperDemoApplication.java
@@ -5,36 +5,24 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
-
-import java.util.Collections;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-
 import ru.xpendence.modelmapperdemo.attributes.OrderStatus;
 import ru.xpendence.modelmapperdemo.entity.Customer;
 import ru.xpendence.modelmapperdemo.entity.Order;
 import ru.xpendence.modelmapperdemo.repository.CustomerRepository;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.Collections;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
 @SpringBootApplication
-@EnableSwagger2
 public class ModelmapperDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ModelmapperDemoApplication.class, args);
     }
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SPRING_WEB).select()
-                .apis(RequestHandlerSelectors.basePackage("ru.xpendence.modelmapperdemo"))
-                .build();
-    }
+
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
@@ -48,19 +36,18 @@ public class ModelmapperDemoApplication {
 
 
     @Resource
-	CustomerRepository repository;
+    CustomerRepository repository;
 
     @PostConstruct
-	private void populateDb()
-	{
+    private void populateDb() {
         for (int i = 0; i < 100; i++) {
-            Customer c = new Customer("Customer"+i, null);
-            Order o = new Order("Order"+i, c, OrderStatus.TODO);
+            Customer c = new Customer("Customer" + i, null);
+            Order o = new Order("Order" + i, c, OrderStatus.TODO);
             c.setOrders(Collections.singletonList(o));
             repository.save(c);
         }
 
-	}
+    }
 
 }
 
---
+++ src/main/java/ru/xpendence/modelmapperdemo/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package ru.xpendence.modelmapperdemo.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2
+@ConditionalOnProperty(name = "app.swagger.exposeAPI", havingValue = "true", matchIfMissing = false)
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SPRING_WEB)
+                .produces(Set.of(MediaType.APPLICATION_JSON_VALUE))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ru.xpendence.modelmapperdemo"))
+                .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Demo",
+                "Demo REST API.",
+                "1.0",
+                null,
+                null,
+                null, null, Collections.emptyList());
+    }
+
+
+}
